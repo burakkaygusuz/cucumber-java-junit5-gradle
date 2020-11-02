b52d5d8a8f1be3323cee84fbf0af3eae207ffb6f
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-11-02 15:04:48 +0300

    Webdriver and WebDriverWait were configured using helper methods

---
+++ src/test/java/com/google/tests/config/DriverFactory.java
@@ -0,0 +1,45 @@
+package com.google.tests.config;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DriverFactory {
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+    public static WebDriver getChromeDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+
+            System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+            Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("enable-automation");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-gpu");
+            options.setHeadless(true);
+            options.setAcceptInsecureCerts(true);
+            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static WebDriverWait getWebDriverWait() {
+        if (wait == null) wait = new WebDriverWait(getChromeDriver(), Duration.ofSeconds(10));
+        return wait;
+    }
+}
