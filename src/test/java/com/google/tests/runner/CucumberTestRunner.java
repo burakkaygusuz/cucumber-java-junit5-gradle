7c09d3e9723755aa3f70b522e18a5b0343f2415e
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-11-02 14:56:27 +0300

    Test runner class with its options is added

---
+++ src/test/java/com/google/tests/runner/CucumberTestRunner.java
@@ -0,0 +1,16 @@
+package com.google.tests.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        glue = "com.google.tests",
+        plugin = {"pretty"},
+        monochrome = true
+)
+public class CucumberTestRunner {
+}
