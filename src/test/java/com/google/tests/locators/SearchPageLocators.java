fa2c713c203bf8312d06a9ea15dfbaf2bc383621
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-11-05 11:03:58 +0300

    Locator updated

--- src/test/java/com/google/tests/locators/SearchPageLocators.java
+++ src/test/java/com/google/tests/locators/SearchPageLocators.java
@@ -3,5 +3,5 @@ package com.google.tests.locators;
 import org.openqa.selenium.By;
 
 public interface SearchPageLocators {
-    By FIRST_LISTED_RESULT = By.xpath("//*[@id=\"rso\"]/div[1]/div/div[1]/a/h3/span");
+    By FIRST_LISTED_RESULT = By.xpath("//body/div[@id='main']/div[@id='cnt']/div[10]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/h3[1]/span[1]");
 }
