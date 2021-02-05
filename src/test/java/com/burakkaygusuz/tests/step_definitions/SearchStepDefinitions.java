190624ee874a43ea80a746a0afbc9b19639fb317
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-02-05 14:03:02 +0300

    Wait for the URL contains the searched value

--- src/test/java/com/burakkaygusuz/tests/step_definitions/SearchStepDefinitions.java
+++ src/test/java/com/burakkaygusuz/tests/step_definitions/SearchStepDefinitions.java
@@ -6,7 +6,10 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static com.burakkaygusuz.tests.config.DriverFactory.getWebDriverWait;
 import static com.burakkaygusuz.tests.pages.HomePage.*;
 import static com.burakkaygusuz.tests.pages.SearchPage.*;
 
@@ -14,6 +17,7 @@ public class SearchStepDefinitions {
 
     HomePage homePage = getHomePage();
     SearchPage searchPage = getSearchPage();
+    WebDriverWait wait = getWebDriverWait();
 
     @Given("I type {string} in searchBar")
     public void iTypeInSearchBar(String value) {
@@ -30,6 +34,7 @@ public class SearchStepDefinitions {
 
         System.out.println("First Google Result: " + searchPage.getFirstListedResultLinkText());
 
+        wait.until(ExpectedConditions.urlContains("&q=Cucumber"));
         Assertions.assertTrue(searchPage.getFirstListedResultLinkText().startsWith(text), ": The text of first google result is different");
     }
 }
