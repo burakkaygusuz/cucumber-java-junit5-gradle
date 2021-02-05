696e5e605e0ff1bb6e5432eccb3052857bcd8acd
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-02-05 14:03:52 +0300

    Some logs added about operations

--- src/test/java/com/burakkaygusuz/tests/pages/BasePage.java
+++ src/test/java/com/burakkaygusuz/tests/pages/BasePage.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BasePage {
 
     private final WebDriverWait wait = DriverFactory.getWebDriverWait();
+    private final Logger log = LoggerFactory.getLogger(BasePage.class.getName());
 
     private BasePage() {
     }
@@ -20,17 +23,22 @@ public class BasePage {
     public BasePage click(By locator) {
         WebElement element = findElementByLocator(locator);
         wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+        log.info(String.format("The web element that has locator '%s' has been clicked", locator));
         return this;
     }
 
     public BasePage clearAndType(By locator, String text) {
         findElementByLocator(locator).clear();
+        log.info(String.format("The web element that has locator: '%s' has been cleared", locator));
         findElementByLocator(locator).sendKeys(text);
+        log.info(String.format("'%s' has been written in the web element that has locator: '%s'", text, locator));
         return this;
     }
 
     public String getText(By locator) {
-        return findElementByLocator(locator).getText();
+        String text = findElementByLocator(locator).getText();
+        log.info(String.format("Received text is: '%s'", text));
+        return text;
     }
 
     private WebElement findElementByLocator(By locator) {
